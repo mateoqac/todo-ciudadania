0ebd164d21393075ab5639846d029e4f5fa1aaa5
Mateo <mate.qac@gmail.com>
2018-07-03 14:24:42 +0200

    comment auth methods

--- src/providers/auth/auth.service.ts
+++ src/providers/auth/auth.service.ts
@@ -23,30 +23,30 @@ export class AuthService {
         return this.afAuth.auth.createUserWithEmailAndPassword(credentials.email, credentials.password);
     }
 
-    signInWithGoogle() {
-		console.log('Sign in with google');
-		return this.oauthSignIn(new firebase.auth.GoogleAuthProvider());
-}
+//     signInWithGoogle() {
+// 		console.log('Sign in with google');
+// 		return this.oauthSignIn(new firebase.auth.GoogleAuthProvider());
+// }
 
-private oauthSignIn(provider: AuthProvider) {
-	if (!(<any>window).cordova) {
-		return this.afAuth.auth.signInWithPopup(provider);
-	} else {
-		return this.afAuth.auth.signInWithRedirect(provider)
-		.then(() => {
-			return this.afAuth.auth.getRedirectResult().then( result => {
-				// This gives you a Google Access Token.
-				// You can use it to access the Google API.
-				let token = result.credential.accessToken;
-				// The signed-in user info.
-				let user = result.user;
-				console.log(token, user);
-			}).catch(function(error) {
-				// Handle Errors here.
-				alert(error.message);
-			});
-		});
-	}
-}
+// private oauthSignIn(provider: AuthProvider) {
+// 	if (!(<any>window).cordova) {
+// 		return this.afAuth.auth.signInWithPopup(provider);
+// 	} else {
+// 		return this.afAuth.auth.signInWithRedirect(provider)
+// 		.then(() => {
+// 			return this.afAuth.auth.getRedirectResult().then( result => {
+// 				// This gives you a Google Access Token.
+// 				// You can use it to access the Google API.
+// 				let token = result.credential.accessToken;
+// 				// The signed-in user info.
+// 				let user = result.user;
+// 				console.log(token, user);
+// 			}).catch(function(error) {
+// 				// Handle Errors here.
+// 				alert(error.message);
+// 			});
+// 		});
+// 	}
+// }
 
 }
